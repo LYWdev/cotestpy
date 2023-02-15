50bc39bebc26dec826f2c6bb8edf81c9ac31f481
lywdev <lywdev@lywdevs-MacBook-Pro.local>
2023-02-15 15:47:36 +0900

    first commit

---
+++ ch01ex/ANewHope.py
@@ -0,0 +1,15 @@
+#wc16c1j1
+#ANewHope
+spky='spky'
+num=int(input())
+if num<1 or num>5:
+  print("err")
+##elif: 
+else: 
+  start="A long time ago in a galaxy"
+
+  far=' far'*num
+  dot=str(','*(num-1))
+
+  end="away"
+  print(start+far+dot+end)
---
+++ ch01ex/ASppokySeason.py
@@ -0,0 +1,10 @@
+#wc16c1j1
+#ANewHope
+spky='spky'
+num=int(input())
+if num<2 or num>20:
+  print("err")
+##elif: 
+else: 
+  o='o'*num
+  print("sp"+o+"ky")
